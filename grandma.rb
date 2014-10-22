8df369169dee11820d10ee8546dcd3040502aaef
chanhpark <chanhyun.park1211@gmail.com>
2014-10-21 21:44:09 -0400

    Deaf Grandma
    deaf grandma

---
+++ grandma.rb
@@ -0,0 +1,20 @@
+puts 'hey honey can you tell me a story?'
+response = gets.chomp
+bye = 0
+
+while bye < 3
+  if response != response.upcase
+  puts 'HUH? SPEAK UP SONNY!'
+  end
+
+  if  (response == response.upcase && response != 'BYE')
+    puts 'no not since ' + (1930+rand(32)).to_s + ' ! '
+  end
+
+    if response == 'BYE'
+      puts ' '
+      bye = bye + 1
+    end
+    response = gets.chomp
+
+  end
